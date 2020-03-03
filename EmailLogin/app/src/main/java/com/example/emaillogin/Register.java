144393502ec46b274a4436b43ff1454a41f99fb9
Sami Idreesi <sami_khalid@hotmail.co.uk>
2020-03-03 04:02:59 +0000

    register user

--- EmailLogin/app/src/main/java/com/example/emaillogin/Register.java
+++ EmailLogin/app/src/main/java/com/example/emaillogin/Register.java
@@ -1,14 +1,103 @@
 package com.example.emaillogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+    //Create variables for the text inputs and buttons on the register page
+    EditText mFullName,mEmail,mPassword;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Connect variables to the resources on the page
+        mFullName = findViewById(R.id.fullName);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.email);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        mLoginBtn = findViewById(R.id.toLogin);
+        progressBar = findViewById(R.id.registerProgressBar);
+
+        //Get the current instance from Firebase so we can perform the various operations
+        fAuth = FirebaseAuth.getInstance();
+
+        //Check if the user is already logged in
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        //When the register button is clicked
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get the email and password from the text input fields
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                //Check if the input for email is empty
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Enter an email");
+                    return;
+                }
+                //Check if the input for password is empty
+                if (TextUtils.isEmpty(password)){
+                    mPassword.setError("Enter a password");
+                    return;
+                }
+                //Check if the length of the password is greater than 6 characters
+                if (password.length() < 6){
+                    mPassword.setError("Password must be greater than 6 characters");
+                    return;
+                }
+                //Make the progress bar visible
+                progressBar.setVisibility(View.VISIBLE);
+
+                //Register the user in Firebase
+
+                //Attempt to register user with email and password and use event listener to determine if the registration was successful or not
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //If the registration is successful
+                        if (task.isSuccessful()){
+                            //Show message to the user that the account has been created
+                            Toast.makeText(Register.this, "User created", Toast.LENGTH_SHORT).show();
+                            //Send user to the main activity
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+                        //If the registration is unsuccessful
+                        else{
+                            //Show error message
+                            Toast.makeText(Register.this, "Error in registeration " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+
+            }
+        });
     }
 }
